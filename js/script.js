9b7ee2a7395ba52f00b627d1a1f71e65c9bcebe2
Artsemi <artsemi.shyshkin@gmail.com>
2022-03-14 21:38:10 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+"use strict";       // работа в современном режиме
+
+let number = 4.6; 
+
+console.log(4/0);           
+                    // выведет Infinity = бесконечность
+console.log('string' * 9);
+                    // выводит NaN
+
+const persone = "Alex";
+
+
+
+
+/** УРОК 1
+ * 
+ *  let number = 5; // изменяемая переменная
+const leftBorderWidth = 1; 
+                    // const - неизменяемая переменная
+
+number = 10;        // изменяем переменную
+console.log(number);// выводим значение переменной
+
+const obj = {      // объект
+    a: 50
+};
+
+obj.a = 10;         // изменяем значение в объекте
+
+console.log(obj);   // выводим объект
+
+console.log(Name);  // особенность var - выводится 
+                    // до того, как объявлена
+var Name = 'Ivan';  // устаревшая переменная
+
+*/
